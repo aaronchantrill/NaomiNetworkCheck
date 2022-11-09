6ab8446ad582143aceaadf6f54b85a279235c25f
Aaron Chantrill <aaron.chantrill@dottywood.org>
2022-11-09 13:59:11 -0500

    Clarified the pronunciation of ipv4 addresses

--- __init__.py
+++ __init__.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
-from netifaces import interfaces, ifaddresses, AF_INET, AF_INET6
 from naomi import plugin
+from naomi import profile
+from netifaces import interfaces, ifaddresses, AF_INET, AF_INET6
+from num2words import num2words
+
+
+def IPv4Address2Words(ip_address):
+    language = profile.get('language', 'en_US')
+    blocks = ip_address.split('.')
+    return " ".join([
+        num2words(blocks[0], lang=language),
+        "dot",
+        num2words(blocks[1], lang=language),
+        "dot",
+        num2words(blocks[2], lang=language),
+        "dot",
+        num2words(blocks[3], lang=language)
+    ])
+
 
 class Plugin(plugin.SpeechHandlerPlugin):
     def intents(self):
@@ -39,7 +56,7 @@ class Plugin(plugin.SpeechHandlerPlugin):
     def handle(self, intent, mic):
         IPAddresses = self.getIPAddresses()
         if len(IPAddresses) < 1:
-            mic.say("No, I am not connected.")
+            mic.say(self.gettext("No, I am not connected."))
         else:
             if len(IPAddresses) == 1:
                 # One interface
@@ -48,32 +65,41 @@ class Plugin(plugin.SpeechHandlerPlugin):
                         # One interface, one protocol
                         # Yes, I am connected to the ipv4 network. My IP address is 10.42.0.120.
                         for protocol in IPAddresses[interface]:
-                            mic.say("Yes, I am connected to the {} network. My IP address is {}.".format(protocol, IPAddresses[interface][protocol]))
+                            IPAddressWords = IPAddresses[interface][protocol]
+                            if protocol == "ipv4":
+                                IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                            mic.say(self.gettext("Yes, I am connected to the {} network. My IP address is {}.").format(protocol, IPAddresses[interface][protocol]))
                     else:
                         # One interface, multiple protocols
                         # Yes, I am connected to the network. My ipv4 address is 10.42.0.120. My ipv6 address is fe80::1.
                         utterance1 = ""
                         for protocol in IPAddresses[interface]:
-                            utterance1 = '{} My {} address is {}.'.format(utterance1, protocol, IPAddresses[interface][protocol])
-                        mic.say("Yes, I am connected to a network. {}".format(utterance1))
+                            IPAddressWords = IPAddresses[interface][protocol]
+                            if protocol == "ipv4":
+                                IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                            utterance1 = self.gettext('{} My {} address is {}.').format(utterance1, protocol, IPAddressWords)
+                        mic.say(self.gettext("Yes, I am connected to a network. {}").format(utterance1))
             else:
                 # multiple interfaces
                 # Yes, I am connected to 2 networks. On the wlan1 interface, my ipv4 address is 192.196.0.12 and my ipv6
-                mic.say("Yes, I am connected to {} networks.".format(len(IPAddresses)))
+                mic.say(self.gettext("Yes, I am connected to {} networks.").format(len(IPAddresses)))
                 for interface in IPAddresses:
                     utterance1 = ""
                     protocol_number = 0
                     for protocol in IPAddresses[interface]:
                         protocol_number += 1
                         if(protocol_number != 1):
-                            utterance1 = "{}and ".format(utterance1)
-                        utterance1 = "{}my {} address is {}".format(utterance1, protocol, IPAddresses[interface][protocol])
-                    mic.say("On the {} interface, {}.".format(interface, utterance1))
+                            utterance1 = self.gettext("{}and ").format(utterance1)
+                        IPAddressWords = IPAddresses[interface][protocol]
+                        if protocol == "ipv4":
+                            IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                        utterance1 = self.gettext("{}my {} address is {}").format(utterance1, protocol, IPAddressWords)
+                    mic.say(self.gettext("On the {} interface, {}.").format(interface, utterance1))
 
     def handle_ipaddress(self, intent, mic):
         IPAddresses = self.getIPAddresses()
         if(len(IPAddresses) < 1):
-            mic.say("I am not currently connected to any networks.")
+            mic.say(self.gettext("I am not currently connected to any networks."))
         elif len(IPAddresses) == 1:
             # One interface
             for interface in IPAddresses:
@@ -81,13 +107,19 @@ class Plugin(plugin.SpeechHandlerPlugin):
                     # One interface, one protocol
                     # My IP address is 10.42.0.120.
                     for protocol in IPAddresses[interface]:
-                        mic.say("My IP address is {}.".format(IPAddresses[interface][protocol]))
+                        IPAddressWords = IPAddresses[interface][protocol]
+                        if protocol == "ipv4":
+                            IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                        mic.say(self.gettext("My IP address is {}.".format(IPAddressWords)))
                 else:
                     # One interface, multiple protocols
                     # My ipv4 address is 10.42.0.120. My ipv6 address is fe80::1.
                     utterance1 = ""
                     for protocol in IPAddresses[interface]:
-                        utterance1 = '{}My {} address is {}. '.format(utterance1, protocol, IPAddresses[interface][protocol])
+                        IPAddressWords = IPAddresses[interface][protocol]
+                        if protocol == "ipv4":
+                            IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                        utterance1 = self.gettext('{}My {} address is {}. ').format(utterance1, protocol, IPAddressWords)
                     mic.say(utterance1)
         else:
             # multiple interfaces
@@ -98,9 +130,12 @@ class Plugin(plugin.SpeechHandlerPlugin):
                 for protocol in IPAddresses[interface]:
                     protocol_number += 1
                     if(protocol_number != 1):
-                        utterance1 = "{}and ".format(utterance1)
-                    utterance1 = "{}my {} address is {}".format(utterance1, protocol, IPAddresses[interface][protocol])
-                mic.say("On the {} interface, {}.".format(interface, utterance1))
+                        utterance1 = self.gettext("{}and ").format(utterance1)
+                    IPAddressWords = IPAddresses[interface][protocol]
+                    if protocol == "ipv4":
+                        IPAddressWords = IPv4Address2Words(IPAddresses[interface][protocol])
+                    utterance1 = self.gettext("{}my {} address is {}").format(utterance1, protocol, IPAddressWords)
+                mic.say(self.gettext("On the {} interface, {}.").format(interface, utterance1))
 
     # https://stackoverflow.com/questions/270745/how-do-i-determine-all-of-my-ip-addresses-when-i-have-multiple-nics
     @staticmethod
